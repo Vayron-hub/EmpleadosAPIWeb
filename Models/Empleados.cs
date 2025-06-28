54f18efa789fb203b0e14fcc907134912a092dec
Vayron-hub <vayron15conchas@gmail.com>
2025-06-28 16:23:20 -0600

    first commit

---
+++ Models/Empleados.cs
@@ -0,0 +1,12 @@
+﻿namespace EmpleadosAPI.Models
+{
+    public class Empleados
+    {
+        public int IdEmpleado { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public string genero { get; set; }
+    }
+}
